753c894bc922032ec88804636001c18f6f2a9b75
HubertBraun <hubert.braun98@gmail.com>
2021-02-15 19:32:26 +0100

    added swagger package

--- MailingApi/Controllers/MailingController.cs
+++ MailingApi/Controllers/MailingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,61 @@ namespace MailingApi.Controllers
 
         public MailingController()
         {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="404">Group not found</response> 
+        /// <response code="200">OK</response> 
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get()
+        {
+            return NotFound();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="409">Group already exist</response> 
+        /// <response code="201">Group created</response> 
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult Post()
+        {
+            return NoContent();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="204">Group not found</response> 
+        /// <response code="201">Group created</response> 
+        /// <response code="200">Group updated</response> 
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Put()
+        {
+            return NoContent();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="404">Group not found</response> 
+        /// <response code="200">Group deleted</response> 
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Delete()
+        {
+            return NotFound();
         }
 
     }
--- MailingApi/Startup.cs
+++ MailingApi/Startup.cs
@@ -26,6 +26,7 @@ namespace MailingApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,14 @@ namespace MailingApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mailing Api V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
