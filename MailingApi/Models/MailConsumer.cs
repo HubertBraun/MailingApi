03e2c262085138e7948d19abb5156443ce8e8a05
HubertBraun <hubert.braun98@gmail.com>
2021-02-16 19:05:21 +0100

    added models

---
+++ MailingApi/Models/MailConsumer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailingApi.Models
+{
+    public class MailConsumer
+    {
+        public string ConsumerAddress { get; set; }
+        public string ConsumerId { get; set; }
+    }
+
+}
---
+++ MailingApi/Models/MailUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailingApi.Models
+{
+    public class MailUser
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
